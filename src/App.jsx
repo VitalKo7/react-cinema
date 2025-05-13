a785079f9d14d45989ffdc45e0948a409dfafc90
VK <vit_ko@ukr.net>
2025-05-13 20:48:16 +0200

    update project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
+import React from 'react';
 import './App.css';
 
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
-import Banner from './components/Home/Banner';
-import Filter from './components/Home/Filter';
-import MoviesList from './components/Movies/MoviesList';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <>
       <Header />
-      <Banner />
-
-      <div className="container">
-        <Filter />
-        <MoviesList />
-
-        <div className="movies__scroll">
-          <button>
-            <i className="fa-solid fa-arrow-down"></i>
-          </button>
-        </div>
-      </div>
+      <Home />
       <Footer />
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Banner from '../components/Home/Banner';
+import Filter from '../components/Home/Filter';
+import MoviesList from '../components/Movies/MoviesList';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Banner />
+
+      <div className="container">
+        <Filter />
+        <MoviesList />
+
+        <div className="movies__scroll">
+          <button>
+            <i className="fa-solid fa-arrow-down"></i>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
