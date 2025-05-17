1af70a37403e8b89e14b440ec75a0a03127c889d
VK <vit_ko@ukr.net>
2025-05-17 21:28:50 +0200

    FilterButtons update

--- src/components/Filter/FilterButtons.jsx
+++ src/components/Filter/FilterButtons.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const FilterButtons = () => {
+  const [active, changeActive] = React.useState('Movies');
+
   let categories = [
     'Movies',
     'Cinema',
@@ -16,7 +18,13 @@ const FilterButtons = () => {
   return (
     <div className="filter__buttons">
       {categories.map((value, index) => (
-        <button key={index}>{value}</button>
+        <button
+          key={index}
+          onClick={() => changeActive(value)}
+          className={value === active ? 'active' : ''}
+        >
+          {value}
+        </button>
       ))}
     </div>
   );
